52e9d625dbdcb2aabddf51522770286f149ca3a0
yokookamoto <wtwt6599@gmail.com>
2020-03-02 09:11:24 +0900

    AWS2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,10 @@
-# config valid only for current version of Capistrano
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '~>3.12.0'
-
-# Capistranoのログの表示に利用する
 set :application, 'chat-space'
-
-# どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yokookamoto/chat-space.git'
 
-# バージョンが変わっても共通で参照するディレクトリを指定
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, %w{ config/secrets.yml }
+
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
@@ -22,6 +16,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
